e50032f5502e60343e8df49aa84b308a6304b5b5
Murphy <murphyspublic@gmail.com>
2012-06-05 12:51:41 -0600

    Before moving to points.

--- js/shallow.js
+++ js/shallow.js
@@ -3,7 +3,11 @@
 */
 
 var cam, scene, renderer, geo, mesh, mat;
-var CPs; // For accessing all of the points on the surface.
+var CPs = []; // For accessing all of the points on the surface.
+
+// Params to change simulation:
+var gridWidth = 50;
+var gridRes = 10;
 
 init();
 play();
@@ -12,23 +16,33 @@ function init() {
     scene = new THREE.Scene();
 
     cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-    cam.position.z = 50;
-    cam.position.y = 50;
-    cam.position.x = 50;
+    cam.position = new THREE.Vector3(50, 50, 50);
     scene.add(cam);
 
-    geo = new THREE.PlaneGeometry(50,50,10,10);
-    mat = new THREE.MeshBasicMaterial({color: 0x505050, wireframe: true});
+    geo = new THREE.PlaneGeometry(gridWidth,gridWidth,gridRes,gridRes);
+    mat = new THREE.MeshBasicMaterial({color: 0x808080, wireframe: true});
     mesh = new THREE.Mesh(geo, mat);
 
     // Set up custom data structures.
-    v = new controlPoint(mesh.geometry.vertices[60]);
-    v.vertex.y += 50;
+    var cols = gridRes + 1;
+    var loopRange = cols * cols - 1;
+    var verts = mesh.geometry.vertices;
+    for (i = loopRange; i >= 0; i--) {
+        var v = new controlPoint(verts[i]);
+        // Set up sibling relationships.
+        if (i % cols !== 0){ v.left = verts[i-1]; } // If we're not the left column.
+        if (i+1 % cols !== 0){ v.right = verts[i+1]; } // If we're not the right column.
+        if (i-cols >= 0){ v.above = verts[i-cols]; } // If we're not the top row.
+        if (i+cols <= loopRange){ v.below = verts[i+cols]; } // If we're not the top row.
+        // Push the point to the front of the array.
+        CPs.unshift(v);
+    }
+
 
     // Set up rendering stuff:
     scene.add(mesh);
     cam.lookAt(mesh.position);
-    renderer = new THREE.CanvasRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 }
@@ -45,5 +59,17 @@ function play() {
 // Define some objects to be used:
 function controlPoint(vert){
     this.vertex = vert;
-    this.name = "hal";
+    this.velU = 0;
+    this.velV = 0;
+    this.curH = this.vertex.y;
+    this.restH = 0;
+    this.left = undefined;
+    this.right = undefined;
+    this.above = undefined;
+    this.below = undefined;
+
+    this.setCurH = function(h){
+        this.curH = h;
+        this.vertex.y = h;
+    };
 }
